1a880ca41ec06aefbbf54181e6365d27d49f10e9
Evelin Garza <eveling045@gmail.com>
2020-08-30 20:58:47 -0500

    Updating Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import Logo from "../Logo/Logo";
 import Searchbar from "../Searchbar/Searchbar";
+import AddMovieModal from "../AddMovieModal/AddMovieModal";
+import PropTypes from "prop-types";
 
 import "./Header.scss";
 
-const Header = () => {
-    return (
-        <header className="header">
-            <div className="container">
-                <div className="top-bar">
-                    <Logo />
-                    <button className="btn-add-movie">+ ADD MOVIE</button>
-                </div>
-                <div className="main-content">
-                    <h2>FIND YOUR MOVIE</h2>
-                    <Searchbar />
-                </div>
-            </div>
-        </header>
-    );
-};
+export default class Header extends Component {
+    openAddMovieModal = () => {
+        this.props.showOrHideModal(true, 'add-movie');
+    }
 
-export default Header;
+    render() {
+        return (
+            <header className="header">
+                <div className="container">
+                    <div className="top-bar">
+                        <Logo />
+                        <button onClick={this.openAddMovieModal} className="btn-add-movie">+ ADD MOVIE</button>
+                    </div>
+                    <div className="main-content">
+                        <h2>FIND YOUR MOVIE</h2>
+                        <Searchbar />
+                    </div>
+                </div>
+                <>
+                    <AddMovieModal
+                        onClose={this.props.showOrHideModal}
+                        show={this.props.isModalOpened && this.props.modalType === 'add-movie'} />
+                </>
+            </header>
+        );
+    }
+}
+
+Header.propTypes = {
+    isModalOpened: PropTypes.bool.isRequired,
+    modalType: PropTypes.string.isRequired,
+    showOrHideModal: PropTypes.func.isRequired
+}
