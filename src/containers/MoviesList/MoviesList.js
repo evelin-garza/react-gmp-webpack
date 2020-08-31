f7767bfdaa598b315cc1d140a1373cce7d7d3a73
Evelin Garza <eveling045@gmail.com>
2020-08-30 21:02:10 -0500

    Updating MoviesList component, adding EditMovieModal and DeleteMovieModal

--- src/containers/MoviesList/MoviesList.js
+++ src/containers/MoviesList/MoviesList.js
@@ -1,69 +1,58 @@
 import React, { Component } from "react";
 import MovieCard from "../../components/MovieCard/MovieCard";
+import EditMovieModal from "../../components/EditMovieModal/EditMovieModal";
+import DeleteMovieModal from "../../components/DeleteMovieModal/DeleteMovieModal";
+import mockData from "../../../public/assets/data.json";
+import PropTypes from "prop-types";
 
 import "./MoviesList.scss";
 
 export default class MoviesList extends Component {
-    constructor() {
-        super();
-        this.state = {
-            moviesList: [
-                {
-                    title: "Inception",
-                    imageUrl: "https://movieposters2.com/images/639314-b.jpg",
-                    genre: "Action & Adventure",
-                    year: "2003",
-                },
-                {
-                    title: "Inception",
-                    imageUrl: "https://movieposters2.com/images/639314-b.jpg",
-                    genre: "Action & Adventure",
-                    year: "2003",
-                },
-                {
-                    title: "Inception",
-                    imageUrl: "https://movieposters2.com/images/639314-b.jpg",
-                    genre: "Action & Adventure",
-                    year: "2003",
-                },
-                {
-                    title: "Inception",
-                    imageUrl: "https://movieposters2.com/images/639314-b.jpg",
-                    genre: "Action & Adventure",
-                    year: "2003",
-                },
-                {
-                    title: "Inception",
-                    imageUrl: "https://movieposters2.com/images/639314-b.jpg",
-                    genre: "Action & Adventure",
-                    year: "2003",
-                },
-                {
-                    title: "Inception",
-                    imageUrl: "https://movieposters2.com/images/639314-b.jpg",
-                    genre: "Action & Adventure",
-                    year: "2003",
-                },
-            ]
-        };
+    state = {
+        currentMovie: {},
+        moviesList: []
+    };
+
+    componentDidMount() {
+        this.setState({ moviesList: mockData });
+    }
+
+    setCurrentMovie = (movie) => {
+        this.setState({ currentMovie: movie });
     }
 
     render() {
         const movies = this.state.moviesList;
+        const currentMovie = this.state.currentMovie;
         return (
             <div className="movies-container">
                 <p className="movies-results"><b>{movies.length}</b> movies found</p>
                 <div className="movies-list">
                     {movies.length &&
                         movies.map((movie, index) => (
-                            <MovieCard key={`${movie.title}-${index}`}
-                                title={movie.title}
-                                imageUrl={movie.imageUrl}
-                                genre={movie.genre}
-                                year={movie.year} />
+                            <MovieCard
+                                key={`${movie.title}-${index}`}
+                                movie={movie}
+                                showOrHideModal={this.props.showOrHideModal}
+                                setCurrentMovie={this.setCurrentMovie} />
                         ))}
                 </div>
+                <>
+                    <EditMovieModal
+                        onClose={this.props.showOrHideModal}
+                        show={this.props.isModalOpened && this.props.modalType === 'edit-movie'}
+                        currentMovie={currentMovie} />
+                    <DeleteMovieModal
+                        onClose={this.props.showOrHideModal}
+                        show={this.props.isModalOpened && this.props.modalType === 'delete-movie'} />
+                </>
             </div>
         );
     }
+}
+
+MoviesList.propTypes = {
+    isModalOpened: PropTypes.bool.isRequired,
+    modalType: PropTypes.string.isRequired,
+    showOrHideModal: PropTypes.func.isRequired
 }
