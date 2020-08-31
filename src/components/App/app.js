0f6922f52e031ea46a8fcc7afe110f77adcc9d4b
Evelin Garza <eveling045@gmail.com>
2020-08-30 21:01:20 -0500

    Passing props to Header and MoviesList components

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "../Header/Header";
 import MoviesList from "../../containers/MoviesList/MoviesList";
 import Footer from "../../components/Footer/Footer";
@@ -8,19 +8,42 @@ import ErrorBoundary from "../../components/ErrorBoundary/ErrorBoundary";
 
 import "./App.scss";
 
-const App = () => {
-    return (
-        <>
-            <Header />
-            <div className="content">
-                <MovieFilters />
-                <ErrorBoundary hasErrors={false}>
-                    <MoviesList />
-                </ErrorBoundary>
-                <Footer><Logo /></Footer>
-            </div>
-        </>
-    )
-}
+export default class App extends Component {
+    state = {
+        isModalOpened: false,
+        modalType: ''
+    };
 
-export default App;
+    showOrHideModal = (isModalOpened, modalType = '') => {
+        this.setState({
+            isModalOpened,
+            modalType
+        });
+        this.setBodyStyleOverflow(isModalOpened);
+    }
+
+    setBodyStyleOverflow = (isModalOpened) => {
+        document.body.style.overflow = (isModalOpened) ? 'hidden' : 'unset';
+    }
+
+    render() {
+        return (
+            <>
+                <Header
+                    isModalOpened={this.state.isModalOpened}
+                    modalType={this.state.modalType}
+                    showOrHideModal={this.showOrHideModal} />
+                <div className="content">
+                    <MovieFilters />
+                    <ErrorBoundary hasErrors={false}>
+                        <MoviesList
+                            isModalOpened={this.state.isModalOpened}
+                            modalType={this.state.modalType}
+                            showOrHideModal={this.showOrHideModal} />
+                    </ErrorBoundary>
+                    <Footer><Logo /></Footer>
+                </div>
+            </>
+        );
+    }
+}
