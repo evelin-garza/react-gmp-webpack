37a63278ee7ddc60f01b6213c8488ec8bdd9db7b
Evelin Garza <eveling045@gmail.com>
2020-09-21 20:37:03 -0500

    Implementing movie filters and sorting

--- src/components/MovieFilters/MovieFilters.js
+++ src/components/MovieFilters/MovieFilters.js
@@ -1,39 +1,58 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { connect } from 'react-redux';
+
+import * as movieActions from "../../actions/movieActions";
 
 import "./MovieFilter.scss";
 
 const MovieFilters = (props) => {
     const sortValues = [
         { value: 'id', label: 'Movie ID' },
-        { value: 'title', label: 'Movie Title' },
-        { value: 'release_date', label: 'Release Date' }
+        { value: 'release_date', label: 'Release Date' },
+        { value: 'vote_average', label: 'Rating' }
+    ];
+
+    const genresArray = [
+        'All',
+        'Romance',
+        'Animation',
+        'Adventure',
+        'Family',
+        'Comedy',
+        'Fantasy',
+        'Action',
+        'Drama'
     ];
 
     const [sortArray] = useState(sortValues);
+    const [filters] = useState(genresArray);
 
     const onSortChange = (event) => {
         const value = event.target.value;
-        props.setSortBy(value);
-        console.log('onSortChange: ' + value);
+        props.sortBy(value, props.genreFilter);
+    };
+
+    const onGenreSelect = (event) => {
+        const option = event.target.innerText.toLowerCase();
+        props.filterByGenre(option);
     };
 
 
-    const { sortBy } = props;
+    const sortBy = 'release_date';
 
     return (
         <div className="movie-filters container">
             <div className="categories">
-                <span className="active">ALL</span>
-                <span>DOCUMENTARY</span>
-                <span>COMEDY</span>
-                <span>HORROR</span>
-                <span>CRIME</span>
+                {filters.length &&
+                    filters.map((filter, index) => (
+                        <button key={index} onClick={onGenreSelect} className={(filter.toLowerCase() == props.genreFilter) ? 'active' : ''}>{filter}</button>
+                    ))}
             </div>
             <div className="sorting">
                 <span>SORT BY </span>
                 <div className="select-sort-container">
-                    <select value={sortBy} onChange={onSortChange}>
+                    <select defaultValue={sortBy} onChange={onSortChange}>
                         {sortArray.length &&
                             sortArray.map((sortItem, index) => (
                                 <option key={index} value={sortItem.value}>{sortItem.label}</option>
@@ -46,7 +65,20 @@ const MovieFilters = (props) => {
 };
 
 MovieFilters.propTypes = {
-    setSortBy: PropTypes.func
+    genreFilter: PropTypes.string
 };
 
-export default MovieFilters;
+function mapStateToProps(state, ownProps) {
+    return {
+        genreFilter: state.genreFilter
+    };
+}
+
+const mapDispatcherToProps = (dispatch) => {
+    return {
+        sortBy: (sortBy, genre) => dispatch(movieActions.getMovies('', sortBy, genre)),
+        filterByGenre: (genre) => dispatch(movieActions.getMoviesByGenre(genre))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatcherToProps)(MovieFilters);
