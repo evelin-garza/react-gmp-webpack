9ea3edfd2982925f5183d7f3a4967f2433e3fd4f
Evelin Garza <eveling045@gmail.com>
2020-09-21 20:29:04 -0500

    Adding actionTypes file

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,8 @@
+export const GET_MOVIES = 'GET_MOVIES';
+export const GET_MOVIES_BY_GENRE = 'GET_MOVIES_BY_GENRE';
+export const GET_MOVIE_BY_ID = 'GET_MOVIE_BY_ID';
+export const CLEAR_SELECTED_MOVIE = 'CLEAR_SELECTED_MOVIE';
+export const ADD_MOVIE = 'ADD_MOVIE';
+export const EDIT_MOVIE = 'EDIT_MOVIE';
+export const DELETE_MOVIE = 'DELETE_MOVIE';
+export const CLEAR_MESSAGE = 'CLEAR_MESSAGE';
