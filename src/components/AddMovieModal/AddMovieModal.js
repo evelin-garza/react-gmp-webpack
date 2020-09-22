4acb49b80fa13e024068e3dfd384ea485666e585
Evelin Garza <eveling045@gmail.com>
2020-09-21 20:32:57 -0500

    Updating AddMovieModal

--- src/components/AddMovieModal/AddMovieModal.js
+++ src/components/AddMovieModal/AddMovieModal.js
@@ -10,11 +10,7 @@ const AddMovieModal = (props) => {
                 <h2 className="modal-title">ADD MOVIE</h2>
             </div>
             <div className="modal-body">
-                <MovieForm />
-            </div>
-            <div className="modal-footer">
-                <button className="btn btn-red-outline">RESET</button>
-                <button className="btn btn-filled-red">SUBMIT</button>
+                <MovieForm mode="add" onClose={props.onClose} />
             </div>
         </Modal>
     );
