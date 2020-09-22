63c3d4a50665d861845fb0ffd37886e18989e57c
Evelin Garza <eveling045@gmail.com>
2020-09-21 20:34:06 -0500

    Implementing delete movie feature

--- src/components/DeleteMovieModal/DeleteMovieModal.js
+++ src/components/DeleteMovieModal/DeleteMovieModal.js
@@ -1,9 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from 'react-redux';
 
 import Modal from "../Shared/Modal/Modal";
+import * as movieActions from "../../actions/movieActions";
 
 const DeleteMovieModal = (props) => {
+
+    const deleteMovie = () => {
+        const { movie } = props;
+        props.deleteMovie(movie.id);
+        props.onClose();
+    }
+
     return (
         <Modal {...props}>
             <div className="modal-header">
@@ -13,15 +22,20 @@ const DeleteMovieModal = (props) => {
                 <p>Are you sure you want to delete this movie?</p>
             </div>
             <div className="modal-footer">
-                <button className="btn btn-filled-red">CONFIRM</button>
+                <button className="btn btn-filled-red" onClick={deleteMovie}>CONFIRM</button>
             </div>
         </Modal>
     );
 }
 
 DeleteMovieModal.propTypes = {
-    onClose: PropTypes.func.isRequired,
-    show: PropTypes.bool.isRequired
+    onClose: PropTypes.func.isRequired
 }
 
-export default DeleteMovieModal;
+const mapDispatcherToProps = (dispatch) => {
+    return {
+        deleteMovie: (movieId) => dispatch(movieActions.deleteMovie(movieId))
+    }
+}
+
+export default connect(null, mapDispatcherToProps)(DeleteMovieModal);
