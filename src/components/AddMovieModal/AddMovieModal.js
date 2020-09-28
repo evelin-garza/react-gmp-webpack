26fe7682bae67cea32f541e4a8e258a12ea101bc
Evelin Garza <eveling045@gmail.com>
2020-09-27 19:08:06 -0500

    Passing initialValues and validationSchema to MovieForm

--- src/components/AddMovieModal/AddMovieModal.js
+++ src/components/AddMovieModal/AddMovieModal.js
@@ -2,15 +2,17 @@ import React from "react";
 
 import Modal from "../Shared/Modal/Modal";
 import MovieForm from "../Shared/MovieForm/MovieForm";
+import { ADD_MOVIE_INITIAL_VALUES, ADD_MOVIE_VALIDATION_SCHEMA } from "../Shared/common";
 
 const AddMovieModal = (props) => {
+
     return (
         <Modal {...props}>
             <div className="modal-header">
                 <h2 className="modal-title">ADD MOVIE</h2>
             </div>
             <div className="modal-body">
-                <MovieForm mode="add" onClose={props.onClose} />
+                <MovieForm mode="add" initialValues={ADD_MOVIE_INITIAL_VALUES} validationSchema={ADD_MOVIE_VALIDATION_SCHEMA} onClose={props.onClose} />
             </div>
         </Modal>
     );
--- src/components/EditMovieModal/EditMovieModal.js
+++ src/components/EditMovieModal/EditMovieModal.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Modal from "../Shared/Modal/Modal";
 import MovieForm from "../Shared/MovieForm/MovieForm";
+import { EDIT_MOVIE_VALIDATION_SCHEMA } from "../Shared/common";
 
 const EditMovieModal = (props) => {
     return (
@@ -10,7 +11,7 @@ const EditMovieModal = (props) => {
                 <h2 className="modal-title">EDIT MOVIE</h2>
             </div>
             <div className="modal-body">
-                <MovieForm movie={props.currentMovie} mode="edit" onClose={props.onClose}></MovieForm>
+                <MovieForm movie={props.currentMovie} mode="edit" validationSchema={EDIT_MOVIE_VALIDATION_SCHEMA} onClose={props.onClose}></MovieForm>
             </div>
         </Modal>
     );
