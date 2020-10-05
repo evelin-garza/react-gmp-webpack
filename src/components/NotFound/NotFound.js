88d1cb512913e31cce4920940212fab01a9f7a32
Evelin Garza <eveling045@gmail.com>
2020-10-04 20:37:47 -0500

    Adding Not Found component

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <>
+            <h2>Page Not Found</h2>
+            <p>404</p>
+            <button>Go Back To Home</button>
+        </>
+    );
+}
+
+export default NotFound;
