fc7cb6a20d9a61cb20d2ce79b903817cddca579e
Evelin Garza <eveling045@gmail.com>
2020-10-04 20:37:12 -0500

    Adding routing in App component

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,25 +1,21 @@
 import React from "react";
-import Header from "../Header/Header";
-import MoviesList from "../../containers/MoviesList/MoviesList";
-import Footer from "../../components/Footer/Footer";
-import Logo from "../../components/Logo/Logo";
-import MovieFilters from "../../components/MovieFilters/MovieFilters";
-import ErrorBoundary from "../../components/ErrorBoundary/ErrorBoundary";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 
 import "./App.scss";
+import Home from "../Home/Home";
 
 const App = () => {
     return (
-        <>
-            <Header />
-            <div className="content">
-                <MovieFilters />
-                <ErrorBoundary hasErrors={false}>
-                    <MoviesList />
-                </ErrorBoundary>
-                <Footer><Logo /></Footer>
-            </div>
-        </>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+            </Switch>
+        </Router>
+
     );
 };
 
