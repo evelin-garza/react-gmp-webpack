12231792ecb3965574d243da6c4c72a6c0c56c0f
Evelin Garza <eveling045@gmail.com>
2020-10-06 12:44:53 -0500

    Updating NotFound component

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,12 +1,27 @@
 import React from "react";
 
+import "./NotFound.scss";
+import Logo from "../Logo/Logo";
+import Footer from "../Footer/Footer";
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
     return (
-        <>
-            <h2>Page Not Found</h2>
-            <p>404</p>
-            <button>Go Back To Home</button>
-        </>
+        <div className="main-container">
+            <div className="topbar">
+                <div className="container">
+                    <Logo />
+                </div>
+            </div>
+            <div className="content">
+                <div className="container">
+                    <h2>Page Not Found</h2>
+                    <p>404</p>
+                    <Link to="/"><button>Go Back To Home</button></Link>
+                </div>
+            </div>
+            <Footer><Logo /></Footer>
+        </div>
     );
 }
 
