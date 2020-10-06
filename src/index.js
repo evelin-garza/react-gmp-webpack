029a9afee33429c54f0390e6acb882243753c15a
Evelin Garza <eveling045@gmail.com>
2020-10-06 12:40:06 -0500

    Removing dispatch movies from index

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import { Provider } from 'react-redux';
 
 import App from './components/App/App';
 import configureStore from './store/configureStore';
-import { getMovies } from './actions/movieActions';
 import "./global.scss";
 
 const store = configureStore();
-store.dispatch(getMovies());
 
 ReactDOM.render(
     <Provider store={store}>
