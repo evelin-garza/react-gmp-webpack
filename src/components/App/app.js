329d0dfbd8208cada003540098ee53a54f24eda4
Evelin Garza <eveling045@gmail.com>
2020-10-06 12:43:08 -0500

    Adding routes to app component

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -7,12 +7,17 @@ import {
 
 import "./App.scss";
 import Home from "../Home/Home";
+import NotFound from "../NotFound/NotFound";
+import MovieDetails from "../MovieDetails/MovieDetails";
 
 const App = () => {
     return (
         <Router>
             <Switch>
                 <Route exact path="/" component={Home} />
+                <Route exact path="/search" component={Home} />
+                <Route exact path="/film/:id" component={MovieDetails} />
+                <Route component={NotFound} />
             </Switch>
         </Router>
 
