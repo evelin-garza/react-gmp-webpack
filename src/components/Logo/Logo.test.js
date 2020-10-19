41009d6aea268ae1b4de90f890b92e79b17e20ff
Evelin Garza <eveling045@gmail.com>
2020-10-19 11:20:06 -0500

    Adding Logo test

---
+++ src/components/Logo/Logo.test.js
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+import Logo from "./Logo";
+
+describe('Logo Component', () => {
+    it('should render logo', () => {
+        const { asFragment } = render(<Logo />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
