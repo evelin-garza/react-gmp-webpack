1353fc8264b8dded5e9bde1ca388bea521e43d57
Evelin Garza <eveling045@gmail.com>
2020-10-19 11:20:23 -0500

    Adding test for Footer component

---
+++ src/components/Footer/Footer.test.js
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom/extend-expect';
+import { render } from "@testing-library/react";
+import React from "react";
+
+import Footer from "./Footer";
+import Logo from "../Logo/Logo";
+
+describe('Footer Component', () => {
+    it('should render footer by text', () => {
+        const { getByText } = render(<Footer><Logo /></Footer>);
+        expect(getByText(/netflix/i)).toBeInTheDocument();
+        expect(getByText(/roulette/i)).toBeInTheDocument();
+    });
+});
